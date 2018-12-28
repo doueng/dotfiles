f00ad42e2c1f68dca61eb4da0e48e56211057396
Douglas Engstrand <doueng09@gmail.com>
2018-12-28 20:45:10 +0100

    moved exports to profile

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -6,12 +6,9 @@ source $ZSH_FOLDER/man.zsh
 source $ZSH_FOLDER/vim.zsh
 source $ZSH_FOLDER/setopt.zsh
 source $ZSH_FOLDER/bindkey.zsh
-source $ZSH_FOLDER/export.zsh
 source $ZSH_FOLDER/alias.zsh
 source $ZSH_FOLDER/functions.zsh
 source $ZSH_FOLDER/completion.zsh
 source $ZSH_FOLDER/prompt.zsh
 source $ZSH_FOLDER/fzf.zsh
 
-# remove path duplicates
-typeset -aU path
