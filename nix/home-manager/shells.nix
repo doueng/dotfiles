d2a330d88946b10e6915c0e5cd7a45c8f6dcfb19
Douglas Engstrand <engstrand@uber.com>
2020-12-15 17:56:27 +0100

    fix fish path

--- nix/home-manager/shells.nix
+++ nix/home-manager/shells.nix
@@ -10,5 +10,5 @@
   programs.fish.enable = true;
 
   # config.fish
-  fish.interactiveShellInit = builtins.readFile "./fish/config.fish";
+  fish.interactiveShellInit = builtins.readFile "$HOME/fish/config.fish";
 }
